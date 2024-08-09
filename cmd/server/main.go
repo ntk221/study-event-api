3e80dc70726e9a2fa4576f35a4c98ce36b5a7042
ntk221 <nittakdub@gmail.com>
2024-08-09 21:31:08 +0900

    add slog

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
 	"study-event-api/internal/server"
 )
+
+func init() {
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stderr, nil)))
+}
 
 func main() {
 	srv := server.NewServer()
